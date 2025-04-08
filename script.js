70a869b6295cf3bd0566c0bdd3abf25b7eba03fd
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2025-04-08 15:29:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ var button = document.getElementById("Button")
 document.querySelector("#Button").onclick = function() {
     localStorage.setItem("Name", userName.value)
     localStorage.setItem("Number", playersCount.value)
-    window.open("http://127.0.0.1:5500/GamePlace.html", "_self")
+    window.open("file:///C:/Users/nikni/Documents/GitCode/fullStack/frontend/WebForBunker/WebForWebForBunker/GamePlace.html", "_self")
 }
