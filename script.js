c5860a7e4f1d5ec5de11029915d61ac6be892f56
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2025-04-29 19:33:36 +0300

    Change

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ var button = document.getElementById("Button")
 document.querySelector("#Button").onclick = function() {
     localStorage.setItem("Name", userName.value)
     localStorage.setItem("Number", playersCount.value)
-    window.open("file:///C:/Users/nikni/Documents/GitCode/fullStack/frontend/WebForBunker/WebForWebForBunker/GamePlace.html", "_self")
+    window.open("file:///C:/Users/nikni/Documents/GitCode/fullStack/frontend/WebForBunker/WebForWebForBunker/GamePlace.htm", "_self")
 }
